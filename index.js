0d90cd12c70e39fab06fb32c70afec23681149d0
Chance Goodwin <cgood1413@gmail.com>
2022-07-05 17:56:18 -0500

    Exercise 3.1 - 3.6 completion

---
+++ index.js
@@ -0,0 +1,82 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+let contacts = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Express app working!");
+});
+
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+app.post("/api/contacts", (req, res) => {
+  const id = contacts.length > 0 ? Math.max(...contacts.map((c) => c.id)) : 0;
+  const contact = req.body;
+  const { name, number } = contact;
+  const hasName = contacts.some((contact) => {
+    const regex = new RegExp(name, "i");
+    return regex.test(contact.name);
+  });
+  if (!name || !number) {
+    return res
+      .status(400)
+      .send({ error: "Name and number are both required." });
+  } else if (hasName) {
+    return res.status(400).send({error: 'That name already exists in your contacts'})
+  }
+
+  contact.id = id + 1;
+  contacts = contacts.concat(contact);
+  res.status(200).json(contact);
+});
+
+app.get("/api/contacts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const contact = contacts.find((contact) => contact.id === id);
+  if (contact) {
+    res.json(contact);
+  }
+  res.status(404).send(`No contacts found with ID of ${id}`);
+});
+
+app.delete("/api/contacts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  contacts = contacts.filter((contact) => contact.id !== id);
+  res.status(204).end();
+});
+
+app.get("/info", (req, res) => {
+  res.send(`<p>Phonebook has info for ${contacts.length} contacts</p>
+    <p>${new Date()}</p>`);
+});
+
+app.listen(PORT, () => {
+  `Server listening on port ${PORT}.`;
+});
